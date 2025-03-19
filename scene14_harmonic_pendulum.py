6fa004196f052f81cfcff942d20ef0a7b06b4858
robin <81658877+robinjes@users.noreply.github.com>
2025-03-18 20:18:44 -0700

    Add files via upload

---
+++ scene14_harmonic_pendulum.py
@@ -0,0 +1,15 @@
+import mujoco
+import mujoco.viewer
+import time
+
+# Load the MuJoCo model
+model = mujoco.MjModel.from_xml_path("harmonic_pendulum.xml")
+data = mujoco.MjData(model)
+
+
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)  # Step simulation forward
+        time.sleep(0.05)  # Delay to slow down the simulation (increase this value for slower time)
+        viewer.sync()  # Update viewer
